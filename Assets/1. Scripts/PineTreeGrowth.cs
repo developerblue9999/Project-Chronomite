44dcdff1568152f55fbb0288ccd03204cef07fc3
developerblue9999 <developerblue9999@gmail.com>
2026-03-03 14:03:40 +0900

    완!성

--- Assets/1. Scripts/PineTreeGrowth.cs
+++ Assets/1. Scripts/PineTreeGrowth.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PineTreeGrowth : MonoBehaviour
 {
@@ -9,6 +11,7 @@ public class PineTreeGrowth : MonoBehaviour
     public Sprite[] pineTreeStages;    // 4단계 소나무 스프라이트 배열
     private int clickCount = 0;        // 클릭 횟수
     private int currentStage = 0;      // 현재 나무 단계
+    public TextMeshProUGUI TMP;
 
     void Start()
     {
@@ -17,31 +20,36 @@ public class PineTreeGrowth : MonoBehaviour
 
     void Update()
     {
-        if (Input.anyKeyDown)
+        if (Input.GetMouseButtonDown(0))
         {
             clickCount++;
+            TMP.text = clickCount.ToString();
             CheckGrowthStage();
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            SceneManager.LoadScene("1. StartScene");
         }
     }
 
     void CheckGrowthStage()
     {
-        if (clickCount >= 20 && currentStage == 0)
+        if (clickCount >= 100 && currentStage == 0)
         {
             currentStage = 1;
             treeImage.sprite = pineTreeStages[currentStage];
         }
-        else if (clickCount >= 50 && currentStage == 1)
+        else if (clickCount >= 200 && currentStage == 1)
         {
             currentStage = 2;
             treeImage.sprite = pineTreeStages[currentStage];
         }
-        else if (clickCount >= 80 && currentStage == 2)
+        else if (clickCount >= 300 && currentStage == 2)
         {
             currentStage = 3;
             treeImage.sprite = pineTreeStages[currentStage];
         }
-        else if (clickCount >= 100 && currentStage == 3)
+        else if (clickCount >= 400 && currentStage == 3)
         {
             currentStage = 4;
             treeImage.sprite = pineTreeStages[currentStage];
